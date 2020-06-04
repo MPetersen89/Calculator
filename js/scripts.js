3824202b80471d83d59bb8cf24b4ce028295889e
Michael <michael_petersen_email@yahoo.com>
2020-06-04 15:46:16 -0700

    add radio selector for functions, modify UI logic for each function.
    Co-authored by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-const add = function(number1, number2) {
+// Business Logic:
+
+function add(number1, number2) {
   return number1 + number2;
-};
-const sub = function(number1, number2) {
+}
+
+function subtract(number1, number2) {
   return number1 - number2;
-};
-const mult = function(number1, number2) {
+}
+
+function multiply(number1, number2) {
   return number1 * number2;
 };
-const div = function(number1, number2) {
+
+function divide(number1, number2) {
   return number1 / number2;
 };
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result1 = add(number1, number2);
-alert("Addition: " + result1);
-const result2 = sub(number1, number2);
-alert("Subtraction: " + result2);
-const result3 = mult(number1, number2);
-alert("Multiplication: " + result3);
-const result4 = div(number1, number2);
-alert("Division: " + result4);
+
+// User Interface Logic:
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
